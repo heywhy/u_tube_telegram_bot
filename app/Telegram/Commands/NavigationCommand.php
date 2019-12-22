9b18ad82adee4bfa13397e6c4e2d2afc55f234af
atanda rasheed <atandarash@gmail.com>
2019-12-22 23:02:31 +0100

    chore(search flow): delete navigation messages on response.

--- app/Telegram/Commands/NavigationCommand.php
+++ app/Telegram/Commands/NavigationCommand.php
@@ -5,6 +5,9 @@ namespace App\Telegram\Commands;
 use App\Enums\NavigationActions;
 use App\Support\CommandHelperTrait;
 use Telegram\Bot\Commands\Command;
+use Telegram\Bot\Exceptions\TelegramOtherException;
+use Telegram\Bot\Exceptions\TelegramResponseException;
+use Telegram\Bot\Objects\Message;
 
 class NavigationCommand extends Command
 {
@@ -30,16 +33,54 @@ class NavigationCommand extends Command
      */
     public function handle($arguments)
     {
+        $message = $this->getMessage();
+
         if (NavigationActions::getInstance($arguments)->is(NavigationActions::Cancel())) {
             $this->setUserState(null);
-            return $this->replyWithMessage(['text' => 'Search cancelled',]);
-            // return $this->triggerCommand('help');
+
+            return $this->telegram->editMessageText([
+                'text' => 'Search cancelled',
+                'message_id' => !is_null($message) ? $message->getMessageId() : null,
+                'chat_id' => !is_null($message) ? $message->getChat()->getId() : null,
+            ]);
         }
 
-        $this->addUserState(['navigation' => $arguments]);
+        $this->addUserState(['navigation' => $arguments,]);
         $this->answerCallbackQuery([
             'text' => 'Navigating...',
         ]);
         $this->triggerCommand('route');
+        $this->deleteMessage($message);
+    }
+
+    /**
+     * Return the message attached to the update.
+     *
+     * @return Message|null
+     */
+    protected function getMessage(): ?Message
+    {
+        return !is_null($callbackQuery = $this->getCallbackQuery($this->getUpdate()))
+            ? $callbackQuery->getMessage() : $this->getUpdate()->getMessage();
+    }
+
+    /**
+     * Delete the navigation message.
+     *
+     * @param Message|null $message
+     * @return void
+     */
+    protected function deleteMessage(?Message $message)
+    {
+        try {
+            $params = [
+                'message_id' => !is_null($message) ? $message->getMessageId() : null,
+                'chat_id' => !is_null($message) ? $message->getChat()->getId() : null,
+            ];
+
+            $this->telegram->deleteMessage($params);
+        } catch (TelegramOtherException|TelegramResponseException $e) {
+            logger()->error($e);
+        }
     }
 }
